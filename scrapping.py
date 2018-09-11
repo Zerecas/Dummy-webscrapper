bbc2d467a2eb59046b1e17eec77f55cc6f2dc868
Devin Lee <40431409+devin-lee@users.noreply.github.com>
2018-09-12 04:44:31 +1000

    Update scrapping.py

--- scrapping.py
+++ scrapping.py
@@ -1,37 +1,33 @@
 from lxml import html
-import requests, re, time, schedule
-import pandas as pd
-import numpy as np
 from datetime import datetime as dt
+import requests, re, time, schedule, pandas as pd, numpy as np
 
-def job():
+def extract():
     print('Processing Time: ', dt.fromtimestamp(time.time()).strftime('%H:%M'))
-    
+            
     webpage = requests.get('https://www.asx.com.au/asx/statistics/indexInfo.do')
     tree = html.fromstring(webpage.content)
 
-    quote = tree.xpath('//td[@nowrap="nowrap"]/text()')
+    quote = tree.xpath('//td[@nowrap ="nowrap"]/text()')
     quote = [re.sub(r'[^0-9]', '', x) for x in quote]
     quote = [x for x in quote if x]
 
-    dft = pd.DataFrame({'ASX': quote}).astype(float)
-    dft['ASX'] = dft.ASX/10
-    dft = dft.loc[~(dft.ASX < 100)].reset_index().drop(columns = ['index']).iloc[0:8, :]
+    c = [float(a)/10 for a in quote[1::2]]
+    close = (list(c))[1::2]
+    d = [float(b)/10 for b in quote[0::2]]
+    last = (list(d))[0::2]
 
-    df0 = dft.iloc[3:4, :].rename(columns = {'ASX' : 'ASX50_Open'}).dropna().reset_index()
-    df1 = dft.iloc[2:3, :].rename(columns = {'ASX' : 'ASX50_Last'}).dropna().reset_index()
-    df2 = dft.iloc[7:8, :].rename(columns = {'ASX': 'ASX200_Open'}).dropna().reset_index()
-    df3 = dft.iloc[6:7, :].rename(columns = {'ASX' : 'ASX200_Last'}).dropna().reset_index()
-    df = pd.concat([df0, df1, df2, df3], axis = 1).drop(columns = ['index'])
+    chg = [(b-a)/a for a,b in zip(close, last)]
+    overall = last + close + chg
 
-    df['ASX200_DGrowth'] = (df.ASX200_Last - df.ASX200_Open)/df.ASX200_Last
-    df['ASX50_DGrowth'] =  (df.ASX50_Last - df.ASX50_Open)/df.ASX50_Last
-    df['Date'] = dt.today().strftime('%Y-%m-%d-%H:%M')
+    df = (pd.DataFrame(data = [overall]).astype(float))
+    df['GMT10'] = dt.today().strftime('%Y-%m-%d-%H:%M')
 
-    with open('ASX_Minute.csv', 'a') as f:
+    with open('ASX3M.csv', 'a') as f:
         df.to_csv(f, header = False)
 
-schedule.every(1).minutes.do(job)
+schedule.every(5).minutes.do(extract)
 while True:
     schedule.run_pending()
-    time.sleep(1)
+    time.sleep(5)
+
